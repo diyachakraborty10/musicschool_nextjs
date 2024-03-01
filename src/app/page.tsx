d5957d3d9df637f296ff98996f1e6c189373361a
Diya-Chakrabory10 <142688949+Diya-Chakrabory10@users.noreply.github.com>
2024-03-01 22:16:44 +0530

    done FeaturedCourse

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FeaturedCourses from "./components/FeaturedCourses";
 import HeroSection from "./components/HeroSection";
 
 
@@ -7,6 +8,7 @@ export default function Home() {
 
 
   <HeroSection/>
+  <FeaturedCourses/>
   
    </main>
   );
